f022d54ec461638e7079b8f35ad9b037cadfa33d
bluesky6667@gmail.com <cjswoek12>
2016-01-04 09:39:49 +0900

    start

--- hnb/src/main/java/com/hnb/mapper/ThemeMapper.java
+++ hnb/src/main/java/com/hnb/mapper/ThemeMapper.java
@@ -2,9 +2,12 @@ package com.hnb.mapper;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.hnb.global.Command;
 import com.hnb.theme.ThemeVO;
 
+@Repository
 public interface ThemeMapper {
 	// C : 추가(회원가입)
 	public int add(ThemeVO theme);
